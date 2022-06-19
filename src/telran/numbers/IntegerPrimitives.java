659543856e2cb86e3827b9e5df85fd93575b92c5
MishaZimuk <mishazimuk@gmail.com>
2022-06-19 16:25:42 +0300

    HW-2

---
+++ src/telran/numbers/IntegerPrimitives.java
@@ -0,0 +1,103 @@
+package telran.numbers;
+
+public class IntegerPrimitives {
+
+	public static byte getMaxByte() {
+		byte res = 1;
+		while(res>0) {
+			res*=2;
+		}
+		return (byte) (res-1);
+	}
+
+	public static byte getMinByte() {
+		return (byte) (getMaxByte()+1);
+	}
+
+	public static int getLengthByte() {
+		long max = getMaxByte();
+		int res = getLength(max);
+		return res;
+	}
+
+	public static char getMaxChar() {
+		char res = 1;
+		while(res>0) {
+			res = (char) (res*2);
+		}
+		return (char) (res-1);
+	}
+
+	public static char getMinChar() {
+		return (char) (getMaxChar()+1);
+	}
+
+	public static int getLengthChar() {
+		long max = getMaxChar();
+		int res = getLength(max);
+		return res;
+	}
+
+	public static short getMaxShort() {
+		short res = 1;
+		while(res>0) {
+			res=(short)(res*2);
+		}
+		return (short)(res-1);
+	}
+
+	public static short getMinShort() {
+		return (short) (getMaxShort()+1);
+	}
+
+	public static int getLengthShort() {
+		long max = getMaxShort();
+		int res = getLength(max);
+		return res;
+	}
+
+	public static int getMaxInt() {
+		int res = 1;
+		while(res>0) {
+			res*=2;
+		}
+		return res-1;
+	}
+
+	public static int getMinInt() {
+		return getMaxInt()+1;
+	}
+
+	public static int getLengthInt() {
+		long max = getMaxInt();
+		int res = getLength(max);
+		return res;
+	}
+
+	public static long getMaxLong() {
+		long res = 1;
+		while(res>0) {
+			res*=2;
+		}
+		return res-1;
+	}
+
+	public static long getMinLong() {
+		return (long) (getMaxLong()+1);
+	}
+
+	public static int getLengthLong() {
+		long max = getMaxLong();
+		int res = getLength(max);
+		return res;
+	}
+	private static int getLength(long x) {
+		int count = 2;
+		while(x>1) {
+			x/=2;
+			count++;
+		}
+		return count/8;
+	}
+	
+}
