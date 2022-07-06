68056626b9e58915e66d10910658f7785e93b404
MishaZimuk <mishazimuk@gmail.com>
2022-07-06 13:56:52 +0300

    Additional 04

--- src/telran/text/Strings.java
+++ src/telran/text/Strings.java
@@ -1,73 +1,52 @@
 package telran.text;
 
 public class Strings {
-	static public String matches(String name1, String name2) {
-		boolean res = true;
-		String[] nameHelp1 = name1.toLowerCase().split(" ");
-		String[] nameHelp2 = name2.toLowerCase().split(" ");
-		for(int i = 0; i<nameHelp2.length; i++) {
-			if(nameHelp1.length == nameHelp2.length && !isPartNameSame(nameHelp1[i], nameHelp2[i])) {
-				return "no match";
-			}
-			if(nameHelp1.length != nameHelp2.length) {
-			res = checkIfLengthsNotSame(nameHelp1, nameHelp2, i);
-			}
-		}
-		
-		return res?"match":"no match";
-	}
-	private static boolean checkIfLengthsNotSame(String[] nameHelp1, String[] nameHelp2, int i) {
-		boolean res = true;
-			int count = 0;
-			do {
-			res = isPartNameSame(nameHelp1[i+count], nameHelp2[i]);
-			count++;
-			}while(res==false && i+count < nameHelp1.length);
-			if(i+count == nameHelp1.length-1 && i!=nameHelp2.length-1) {
-				 res = false;
-			}
-		return res;
-	}
-	static boolean isPartNameSame(String partName1, String partName2) {
-		boolean res = true;
-		int partNameLength1 = partName1.length();
-		int partNameLength2 = partName2.length();
-		if(partNameLength1!=partNameLength2 && partNameLength1!=1 && partNameLength2!=1) {
-			res = false;
-		}else if(partNameLength1==1 || partNameLength2==1) {
-			res = partName1.charAt(0)==partName2.charAt(0)?true:false;
-		}else {
-		res=isSame(partName1,partName2,partNameLength1);
-		}
-		return res;
-	}
-	private static boolean isSame(String partName1, String partName2, int partNameLength) {
-		for(int i = 0; i<partNameLength; i++) {
-			if(partName1.charAt(i)!=partName2.charAt(i)) {
-				return false;
+	public static boolean hasaValidBrackets(String text) {
+		char[] textChar = text.toCharArray();
+		char[] brOpen = { '(', '[', '{' };
+		char[] brClose = { ')', ']', '}' };
+		StringBuilder orderOpenedBr = new StringBuilder();
+		for (int i = 0; i < textChar.length; i++) {
+			if (!checkIfItOpenBr(brOpen, orderOpenedBr, textChar[i])) {
+				if (!checkIfItCloseBr(brClose, orderOpenedBr, textChar[i])) {
+					return false;
+				}
 			}
 		}
+		if (orderOpenedBr.length() != 0)
+			return false;
 		return true;
 	}
-//Task2
-	static public void sortStringsAsNumbers(String[] strNumbers) {
-		int[] helper = new int[1000];
-		fillHelper(strNumbers, helper);
-		fillMainArray(strNumbers, helper);
-		
-	}
-	private static void fillMainArray(String[] strNumbers, int[] helper) {
-		for(int i = 0, j = 0; i<helper.length; i++) {
-			while(helper[i]>0) {
-				strNumbers[j] = String.valueOf(i);
-				j++;
-				helper[i]--;
+
+	private static boolean checkIfItCloseBr(char[] brClose, StringBuilder orderOpenedBr, char c) {
+		boolean isItPossible = true;
+		for (int i = 0; i < brClose.length; i++) {
+			if (c == ')' || c == ']' || c == '}') {
+				isItPossible = orderOpenedBr.length() == 0 ? false : removeIfCouple(orderOpenedBr, c);
 			}
 		}
+		return isItPossible;
 	}
-	private static void fillHelper(String[] strNumbers, int[] helper) {
-		for(int i = 0; i<strNumbers.length; i++) {
-			helper[Integer.parseInt(strNumbers[i])]++;
+
+	private static boolean checkIfItOpenBr(char[] brOpen, StringBuilder orderOpenedBr, char c) {
+		boolean ifItOpBr = true;
+		for (int i = 0; i < brOpen.length; i++) {
+			if (c != '(' && c != '[' && c != '{') {
+				ifItOpBr = false;
+			} else {
+				orderOpenedBr.append(c);
+			}
 		}
-	}	
+		return ifItOpBr;
 	}
+
+	private static boolean removeIfCouple(StringBuilder orderOpenedBr, char c) {
+		if ((c == ')' && orderOpenedBr.charAt(orderOpenedBr.length() - 1) == '(')
+				|| (c == ']' && orderOpenedBr.charAt(orderOpenedBr.length() - 1) == '[')
+				|| (c == '}' && orderOpenedBr.charAt(orderOpenedBr.length() - 1) == '{')) {
+			orderOpenedBr.deleteCharAt(orderOpenedBr.length() - 1);
+			return true;
+		}
+		return false;
+	}
+}
--- src/telran/text/test/StringClassTests.java
+++ src/telran/text/test/StringClassTests.java
@@ -12,42 +12,10 @@ import telran.text.Strings;
 class StringClassTests {
 
 	@Test
-	void matchesTest() {
-		String name1 = "David";
-		String name2 = "david";
-		String name3 = "John F";
-		String name4 = "John Fitzgerald";
-		String name5 = "John K";
-		String name6 = "Anna Maria Magdalena";
-		String name7 = "Anna Magdalena";
-		String name8 = "Maria Magdalena";
-		String name9 = "Anna Maria Roberta";
-		String name10 = "Anna Magdalena Roberta";
-		String name11 = "Anna Magdalena Maria";
-		assertEquals("match", Strings.matches(name1, name2));
-		assertEquals("match", Strings.matches(name3, name4));
-		assertEquals("no match", Strings.matches(name5, name4));
-		assertEquals("match", Strings.matches(name6, name7));
-		assertEquals("match", Strings.matches(name6, name8));
-		assertEquals("no match", Strings.matches(name6, name9));
-		assertEquals("no match", Strings.matches(name6, name10));
-		assertEquals("no match", Strings.matches(name6, name11));
-		
-		//        Examples:
-//	        - David vs. david: match
-//	        - John F vs. John Fitzgerald: match
-//	        - John K vs. John Fitzgerald: no match
-//	        - Anna Maria Magdalena vs. Anna Magdalena: match
-//	        - Anna Maria Magdalena vs. Maria Magdalena: match
-//	        - Anna Maria Magdalena vs. Anna Maria Roberta: no match
-//	        - Anna Maria Magdalena vs. Anna Magdalena Roberta: no match
-//	        - Anna Maria Magdalena vs. Anna Magdalena Maria: no match
-	}
-	@Test
-	void sortStringsAsNumbersTest() {
-		String[] array1 = new String[]{"10", "234", "2", "987", "1", "34", "99", "34", "99", "34"};
-		String[] expected = new String[] {"1", "2","10", "34", "34", "34", "99", "99", "234", "987"};
-		Strings.sortStringsAsNumbers(array1);
-		assertTrue(Arrays.equals(array1, expected));
+	void hasaValidBracketsTest() {
+		assertTrue(Strings.hasaValidBrackets("aaaaaa (sdfsdfdsf[dfd(f)f] zcvzxcv {{[ghjk]}} asd )"));
+		assertFalse(Strings.hasaValidBrackets(")dfgswfgsf("));
+		assertFalse(Strings.hasaValidBrackets("[werwert(wertwrtw] wertrt)"));
+		assertFalse(Strings.hasaValidBrackets("aaaaaa (sdfsdfdsf[dfd(f)f] zcvzxcv {{[ghjk]}} asd ){"));
 	}
 }
